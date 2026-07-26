510fc5b7fb213675cd3c1e2dcb5ae61063640454
thedanielnunez12 <thedanielnunez12@gmail.com>
2026-07-26 16:15:37 -0400

    Update nba_data.py

--- nba_data.py
+++ nba_data.py
@@ -2,19 +2,32 @@ from nba_api.stats.endpoints import leaguegamefinder
 from nba_api.stats.endpoints import commonplayerinfo
 from nba_api.stats.static import players
 from nba_api.stats.endpoints import playergamelog
+import pandas as pd
+import time
 
 
 def get_team_games(team_id):
-    games = leaguegamefinder.LeagueGameFinder(
-        team_id_nullable=team_id
-    )
 
-    games_df = games.get_data_frames()[0]
+    try:
+        time.sleep(1)
 
-    return games_df
+        games = leaguegamefinder.LeagueGameFinder(
+            team_id_nullable=team_id,
+            timeout=60
+        )
+
+        games_df = games.get_data_frames()[0]
+
+        return games_df
+
+    except Exception as e:
+        print("Team data error:", e)
+        return pd.DataFrame()
+
 
 
 def search_player(player_name):
+
     all_players = players.get_players()
 
     results = [
@@ -25,21 +38,40 @@ def search_player(player_name):
     return results
 
 
-def get_player_info(player_id):
-    info = commonplayerinfo.CommonPlayerInfo(
-        player_id=player_id
-    )
 
-    return info.get_data_frames()[0]
+def get_player_info(player_id):
+
+    try:
+
+        info = commonplayerinfo.CommonPlayerInfo(
+            player_id=player_id,
+            timeout=60
+        )
+
+        return info.get_data_frames()[0]
+
+    except Exception as e:
+        print("Player info error:", e)
+        return pd.DataFrame()
+
 
 
 def get_player_games(player_id):
 
-    games = playergamelog.PlayerGameLog(
-        player_id=player_id
-    )
+    try:
 
-    return games.get_data_frames()[0]
+        games = playergamelog.PlayerGameLog(
+            player_id=player_id,
+            timeout=60
+        )
+
+        return games.get_data_frames()[0]
+
+    except Exception as e:
+        print("Player games error:", e)
+        return pd.DataFrame()
+
+
 
 if __name__ == "__main__":
 
